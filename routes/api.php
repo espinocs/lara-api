b1fd0f29bf9571af70a9adc822f818896d2395a7
Jayvee Santos <santosjay952@gmail.com>
2021-10-25 13:26:44 +0800

    update route

--- routes/api.php
+++ routes/api.php
@@ -27,9 +27,9 @@ Route::post("/register", [AuthController::class, 'register']);
 Route::post("/login", [AuthController::class, 'login']);
 
 //PROTECTED route
-// Route::group(['middleware' => 'auth:sanctum'], function(){
+Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::post("/products",[ProductController::class, 'store']);
     Route::put("/products/{id}",[ProductController::class, 'update']);
     Route::delete("/products/{id}",[ProductController::class, 'destroy']);
     Route::post("/logout", [AuthController::class, 'logout']);
-// });
+});
